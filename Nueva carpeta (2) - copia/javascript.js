42805b674fb3ba12f96faca2340cfe5a1a3d381b
Karlars2005 <karlarios759@gmail.com>
2024-11-20 12:16:34 -0600

    Add files via upload

---
+++ Nueva carpeta (2) - copia/javascript.js
@@ -0,0 +1,15 @@
+document.addEventListener("keydown",function(event){ 
+
+    if (event.key==="a") {
+
+        console.log("Hola mundo");
+
+    }
+
+    if(event.key==="b") {
+
+        console.log("Nos vemos");
+
+    }
+
+});
